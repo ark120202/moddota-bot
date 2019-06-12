2d6b9dd8bb4b502adcdfa65fab0cd9ad25e442e3
ark120202 <ark120202@gmail.com>
2019-06-12 21:39:38 +0500

    feat: temporary disable forum notifications

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { customGameUpdatesLoop } from './bots/custom-game-updates';
-import { forumNotificationsLoop } from './bots/forum-notifications';
 import { newCustomGamesLoop } from './bots/new-custom-games';
 import { redditLoop } from './bots/reddit';
 import { login } from './discord';
@@ -7,12 +6,7 @@ import { login } from './discord';
 (async () => {
   if (process.env.BOT_TOKEN == null) throw new Error('BOT_TOKEN is not defined');
   await login(process.env.BOT_TOKEN);
-  await Promise.all([
-    customGameUpdatesLoop(),
-    forumNotificationsLoop(),
-    newCustomGamesLoop(),
-    redditLoop(),
-  ]);
+  await Promise.all([customGameUpdatesLoop(), newCustomGamesLoop(), redditLoop()]);
 })().catch(err => {
   console.error(err);
   process.exit(1);
