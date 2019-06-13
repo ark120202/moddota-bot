8acd5ab98b7bbb2aed6cdd6f0f44ed3699aadfc7
ark120202 <ark120202@gmail.com>
2019-06-13 23:27:21 +0500

    Revert "feat: temporary disable forum notifications"
    This reverts commit 2d6b9dd8bb4b502adcdfa65fab0cd9ad25e442e3.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { customGameUpdatesLoop } from './bots/custom-game-updates';
+import { forumNotificationsLoop } from './bots/forum-notifications';
 import { newCustomGamesLoop } from './bots/new-custom-games';
 import { redditLoop } from './bots/reddit';
 import { login } from './discord';
@@ -6,7 +7,12 @@ import { login } from './discord';
 (async () => {
   if (process.env.BOT_TOKEN == null) throw new Error('BOT_TOKEN is not defined');
   await login(process.env.BOT_TOKEN);
-  await Promise.all([customGameUpdatesLoop(), newCustomGamesLoop(), redditLoop()]);
+  await Promise.all([
+    customGameUpdatesLoop(),
+    forumNotificationsLoop(),
+    newCustomGamesLoop(),
+    redditLoop(),
+  ]);
 })().catch(err => {
   console.error(err);
   process.exit(1);
